7b0a6b5cac97f3f96468c770a0300ec741ed2c71
fjahangiri <fatemejahangiri1377@gmail.com>
2019-04-25 22:46:41 +0430

    handshake

--- codes/TCPServerSocketImpl.java
+++ codes/TCPServerSocketImpl.java
@@ -1,6 +1,7 @@
 
 import java.net.DatagramPacket;
 import java.net.InetAddress;
+import java.util.Random;
 
 
 public class TCPServerSocketImpl extends TCPServerSocket {
@@ -8,9 +9,14 @@ public class TCPServerSocketImpl extends TCPServerSocket {
     int port;
     InetAddress ip;
     EnhancedDatagramSocket edSocket;
+    int seq; 
+    Random rand=new Random();
+    
 
     public TCPServerSocketImpl(int port) throws Exception {
         super(port);
+        //this.seq=rand.nextInt(100)+100;
+        this.seq=100;
         this.port = port;
         this.ip = InetAddress.getLocalHost(); 
         edSocket = new EnhancedDatagramSocket(port);
@@ -19,14 +25,28 @@ public class TCPServerSocketImpl extends TCPServerSocket {
     @Override
     public TCPSocket accept() throws Exception {
         byte recvBuf[] = new byte[1480]; //Not sure
-        DatagramPacket DpReceive = new DatagramPacket(recvBuf, recvBuf.length, ip, port);
+        DatagramPacket DpReceive = new DatagramPacket(recvBuf, recvBuf.length);
         edSocket.receive(DpReceive);
+        TCPParser ParseDpReceive=new TCPParser(DpReceive);
+        byte sendBuff[] = new byte[0];
+        System.out.println(ParseDpReceive.getSeq());
+        TCPParser AckDpReceive=new TCPParser
+        (sendBuff, sendBuff.length, DpReceive.getAddress(), DpReceive.getPort(), this.seq, ParseDpReceive.getSeq()+1);
+        edSocket.send(AckDpReceive.datagramPacket);
+        byte recvAckBuf[] = new byte[1480]; //Not sure
+        DatagramPacket DpAckReceive = new DatagramPacket(recvAckBuf, recvAckBuf.length);
+        edSocket.receive(DpAckReceive);
+        TCPParser x=new TCPParser(DpAckReceive);
+        System.out.println(x.getAck());
+        return new TCPSocketImpl(DpAckReceive.getAddress().getHostAddress(),DpAckReceive.getPort());
+       //to Doooo check and port and address similarities
 
+        /*
         TCPSocket tcpSocket = new TCPSocketImpl(DpReceive.getAddress().getHostAddress(), DpReceive.getPort());
 
         byte sendBuff[] = new byte[1480];
         DatagramPacket DpSend = new DatagramPacket(sendBuff, sendBuff.length, DpReceive.getAddress(), DpReceive.getPort());
-
+*/
     }
 
     @Override
--- codes/TCPSocketImpl.java
+++ codes/TCPSocketImpl.java
@@ -1,6 +1,10 @@
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.util.Random;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class TCPSocketImpl extends TCPSocket {
     InetAddress ip;
@@ -10,32 +14,63 @@ public class TCPSocketImpl extends TCPSocket {
     Random rand=new Random();
     public TCPSocketImpl(String ip, int port) throws Exception {
         super(ip, port);
-        this.seq=rand.nextInt(100)+100;
+        //this.seq=rand.nextInt(100)+100;
+        this.seq=500;
         this.port = port;
         this.ip = InetAddress.getLocalHost(); 
         edSocket = new EnhancedDatagramSocket(0);
         byte bufSendPacketSYN[]=new byte[0];
-        DatagramPacket sendPacketSYN = new TCPParser(bufSendPacketSYN, 0, this.ip, port,this.seq , 0).datagramPacket;
+        DatagramPacket sendPacketSYN = new TCPParser(bufSendPacketSYN, 0, this.ip, this.port,this.seq , 0).datagramPacket;
         edSocket.send(sendPacketSYN);
         byte bufRecievepacketSYNAC[]=new byte[1148];
         DatagramPacket recievepacketSYNAC=new DatagramPacket(bufRecievepacketSYNAC,bufRecievepacketSYNAC.length);
         edSocket.receive(recievepacketSYNAC);
         TCPParser SYNACparse=new TCPParser(recievepacketSYNAC);
-        //TODOOO handle receive timer and check ac number 
-        // ac=SYNACparse.getAC();
-        // seq=SYNACparse.getSEQ();
-        // byte bufSendPacketAC[]=new byte[0];
-        // DatagramPacket sendPacketAC = new TCPParser(bufSendPacketAC, 0, this.ip, port,seq ,ac+1).datagramPacket;
-        // edSocket.send(sendPacketAC);
+        //TODOOO handle receive timer
+        //check acknowledge
+        int ack=SYNACparse.getSeq();
+        this.seq=SYNACparse.getAck();
+        System.out.println(ack);
+        System.out.println(this.seq);
+        byte bufSendPacketAC[]=new byte[0];
+        DatagramPacket sendPacketAC = new TCPParser(bufSendPacketAC, 0, this.ip, this.port,this.seq ,ack+1).datagramPacket;
+        edSocket.send(sendPacketAC);
     }
 
     @Override
     public void send(String pathToFile) throws Exception {
+        File file=new File("../file.txt");
+        FileInputStream fileIS=null;
+        byte fileContent[]=null;
+        try{
+            fileIS=new FileInputStream(file);
+            fileContent= new byte[(int)file.length()];
+            fileIS.read(fileContent);
+        }
+        catch(FileNotFoundException ex){
+            System.out.println("File not existed: "+ex);
+        }
+        catch(IOException ex){
+            System.out.println("Exception while reading file: "+ex);
+        }
+        finally{
+            try{
+                if(fileIS!=null){
+                    fileIS.close();
+                }
+            }
+            catch(IOException ex){
+                System.out.println("Exception while closing file: "+ ex);
+            }
+        }
+        for(int i=0 ;i<Math.ceil(fileContent.length/1480f);i++){
+            
+        }
 
 //   throw new RuntimeException("Not implemented!");
 
     }
-
+    
     @Override
     public void receive(String pathToFile) throws Exception {
         throw new RuntimeException("Not implemented!");
@@ -43,7 +78,8 @@ public class TCPSocketImpl extends TCPSocket {
 
     @Override
     public void close() throws Exception {
-        throw new RuntimeException("Not implemented!");
+        //throw new RuntimeException("Not implemented!");
+        this.edSocket.close();
     }
 
     @Override
